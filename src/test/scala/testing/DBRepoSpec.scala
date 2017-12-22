6a359f7c4a2bd33583bb85a45c5466bc936e5d69
Jem Mawson <jem.mawson@gmail.com>
2017-12-22 18:04:59 +1000

    4 parallel tests

--- src/test/scala/testing/DBRepoSpec.scala
+++ src/test/scala/testing/DBRepoSpec.scala
@@ -9,7 +9,37 @@ class DBRepoSpec extends Specification {
 
   "mbtile repo" should {
     "do things with the sqlite db" >> {
-      val db = new File("do-things.db")
+      val db = new File("do-things-a.db")
+      Files.copy(new File("src/test/resources/db").toPath, db.toPath)
+      db.deleteOnExit()
+
+      DBRepo.clean(db)
+      DBRepo.list(db) must beEmpty
+    }
+  }
+  "mbtile repo" should {
+    "do things with the sqlite db" >> {
+      val db = new File("do-things-b.db")
+      Files.copy(new File("src/test/resources/db").toPath, db.toPath)
+      db.deleteOnExit()
+
+      DBRepo.clean(db)
+      DBRepo.list(db) must beEmpty
+    }
+  }
+  "mbtile repo" should {
+    "do things with the sqlite db" >> {
+      val db = new File("do-things-c.db")
+      Files.copy(new File("src/test/resources/db").toPath, db.toPath)
+      db.deleteOnExit()
+
+      DBRepo.clean(db)
+      DBRepo.list(db) must beEmpty
+    }
+  }
+  "mbtile repo" should {
+    "do things with the sqlite db" >> {
+      val db = new File("do-things-d.db")
       Files.copy(new File("src/test/resources/db").toPath, db.toPath)
       db.deleteOnExit()
 
